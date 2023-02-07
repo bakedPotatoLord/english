ef4b1b911567bfff6258dec277405b05b36b8360
bakedPotatoLord <918780@dpsk12.net>
2023-02-07 11:06:27 -0700

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<script lang="ts">
+
+  export const app ={
+    name: "App",
+    components:{}
+  }
+</script>
+
+<template>
+  <div class="head">
+    <h1>Josiah's english stuff</h1>
+  </div>
+  <div class="body">
+    <router-view></router-view>
+  </div>
+</template>
+
+<style scoped>
+  body{
+    padding:none
+  }
+  .head{
+    background-color: aqua;
+    padding: 10px
+  }
+
+  
+</style>
