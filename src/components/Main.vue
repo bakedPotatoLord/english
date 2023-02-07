4c54ec1610954e8e4d42ec4787d825298f2d4b56
bakedPotatoLord <918780@dpsk12.net>
2023-02-07 11:06:34 -0700

    create main page

---
+++ src/components/Main.vue
@@ -0,0 +1,23 @@
+
+<template>
+  <div class="body">
+    <table>
+      <tr>
+        <th>Memoirs</th>
+        <td>thing 1</td>
+        <td>thing 1</td>
+      </tr>
+    </table>
+  </div>
+</template>
+
+<style scoped>
+  th{
+    border: solid 2px black;
+  }
+
+  th,
+  td{
+    padding: 2em;
+  }
+</style>
