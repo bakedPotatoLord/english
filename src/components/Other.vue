a292db2829be7465836288a98e1df46958799e84
bakedPotatoLord <918780@dpsk12.net>
2023-02-07 11:06:43 -0700

    create other page

---
+++ src/components/Other.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div class="body">
+    this is the other thing
+  </div>
+</template>
+
+<style scoped>
+  th{
+    border: solid 2px black;
+  }
+
+  th,
+  td{
+    padding: 2em;
+  }
+</style>
