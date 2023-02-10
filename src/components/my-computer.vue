e90e6731af06a2e10c6c0e7984feb1228c7034fc
= <918780@dpsk12.net>
2023-02-09 20:22:29 -0700

    add 2nd story

---
+++ src/components/my-computer.vue
@@ -0,0 +1,26 @@
+
+<template>
+  <div class="body">
+    <h3>My Computer</h3>
+    <p>
+      I love my computer. Some think of it as an ordinary object, but I know that it is much more than that. Its transistors demonstrate beauty and perfection. Cut from precious minerals, they operate individually, flipping back and forth with sacred speed, only attainable by the ascended machine, but when combined, they reach new levels of divinity. It sends signals back and forth across lines of silver, like messengers sprinting across the cobblestone roads of old. It reads and comprehends my mind through my fingers, and from the global network of knowledge, it finds me only the greatest of memes. 
+<br><br>
+It solves problems that my ancestors could only dream of solving. Calculating PI, math’s most notorious irrational constant, used to take men years, only to arrive at 500 digits, with errors nullifying the data only halfway through. Now, through the power of my machine, I can calculate millions of digits, with the push of a key. No problem is outside the scope of the machine.
+</p>
+  </div>
+</template>
+
+<style scoped>
+  .body{
+    margin-left: 10%;
+    margin-right: 10%;
+  }
+
+  .body{
+    font-family: 'Lucida Sans', 'Lucida Sans Regular', 'Lucida Grande', 'Lucida Sans Unicode', Geneva, Verdana, sans-serif;
+  }
+
+  .body p{
+    line-height: 175%;
+  }
+</style>
